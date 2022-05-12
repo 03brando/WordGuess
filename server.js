7a64d1fb0e76b8a974bd4f0537fb70d06192f2fd
03brando <bkandola98@gmail.com>
2022-05-12 18:03:32 -0400

    code cleanup

--- server.js
+++ server.js
@@ -6,9 +6,7 @@ var HTTP_PORT = process.env.PORT || 8080;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-
 const onHttpStart = () => console.log(`HTTP server is listening on port ${HTTP_PORT}`)
-
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname,"/views/index.html"));
